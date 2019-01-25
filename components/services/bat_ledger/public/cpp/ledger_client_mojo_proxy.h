7e4de77fa166423b5b283a6f8c3e32d7a5c7e066
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-01-25 11:07:11 -0500

    fixes after merge with android branch

--- components/services/bat_ledger/public/cpp/ledger_client_mojo_proxy.h
+++ components/services/bat_ledger/public/cpp/ledger_client_mojo_proxy.h
@@ -141,6 +141,7 @@ class LedgerClientMojoProxy : public mojom::BatLedgerClient,
   void SetConfirmationsIsReady(const bool is_ready) override;
 
   void ConfirmationsTransactionHistoryDidChange() override;
+  void OnGrantViaSafetynetCheck(const std::string& nonce) override;
 
   void GetPendingContributions(
       GetPendingContributionsCallback callback) override;
